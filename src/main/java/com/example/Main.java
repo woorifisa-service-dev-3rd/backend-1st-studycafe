362f338b7b332439a2239564d64e82717d0442fa
seonmin5 <seonmin5555@gmail.com>
2024-08-21 20:24:30 +0900

    fix 예약완료 반복 및 필요 없는 출력문 삭제

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -36,14 +36,12 @@ public class Main {
 
         // user login
         User user;
-        user = userService.login();
+        user = userService.login(scanner);
 
         List<Seat> seats = seatDAO.generateSeats();
         seatDAO.displaySeatLayout(seats, reservationDAO);
 
-
         // user
-
         int selectedOption = -1;
         while (selectedOption != 1) {
             System.out.println("이용하실 서비스를 선택해주세요.");
@@ -61,7 +59,6 @@ public class Main {
                     System.out.println("이용할 좌석 번호를 입력하세요:");
                     int selectedSeatId = scanner.nextInt();
                     seat = seatService.selectSeat(selectedSeatId);
-                    System.out.println(seat);
                 }
 
                 // 단체석일 경우 인원 수 입력
@@ -102,74 +99,12 @@ public class Main {
                 // 예약 생성
                 Reservation reservation = reservationService.prepareReservation(userUid, seat.getSeatId(), userService.getTimeInHours(selectedPriceType));
                 reservationService.createReservation(reservation);
-
                 userService.deductTime(userUid, selectedPriceType);
-
-                System.out.println("예약이 완료되었습니다.");
             }
             else {
                 System.out.println("정확한 번호를 입력해주세요.");
             }
         }
-
-
-        // 사용자 UID 입력
-//        System.out.println("사용자 UID를 입력하세요:");
-//        int userUid = scanner.nextInt();
-//
-//        // 좌석 선택
-//        Seat seat = null;
-//        while (seat == null) {
-//            System.out.println("이용할 좌석 번호를 입력하세요:");
-//            int selectedSeatId = scanner.nextInt();
-//            seat = seatService.selectSeat(selectedSeatId);
-//            System.out.println(seat);
-//        }
-//
-//        // 단체석일 경우 인원 수 입력
-//        boolean isGroupSeatValid = true;
-//        if (seat.getSeatType() == SeatType.GROUP) {
-//            while (true) {
-//                System.out.println("인원 수를 입력하세요:");
-//                int groupSize = scanner.nextInt();
-//                isGroupSeatValid = seatService.validateGroupSeat(seat, groupSize);
-//                if (isGroupSeatValid) {
-//                    break;
-//                }else if(!isGroupSeatValid){
-//                    System.out.println("단체석 예약에 실패했습니다.");
-//                    return;
-//                }
-//            }
-//        }
-//
-//        // 사용 시간 선택 및 가격 타입 결정
-//        PriceType selectedPriceType = null;
-//        while (true) {
-//            System.out.println("몇 시간을 이용하시겠습니까? (1시간/2시간/3시간)");
-//            String selectedTime = scanner.next();
-//            try {
-//                selectedPriceType = PriceType.fromTime(selectedTime.trim());
-//
-//                if (userService.hasEnoughTime(userUid, selectedPriceType)) {
-//                    System.out.println(selectedPriceType.getTime() + " 선택되었습니다. 가격은 " + selectedPriceType.getPrice() + "원 입니다.");
-//                    break;
-//                } else {
-//                    System.out.println("보유 시간이 부족합니다. 다시 선택해 주세요.");
-//                }
-//            } catch (IllegalArgumentException e) {
-//                System.out.println("잘못된 시간 선택입니다. 다시 선택해 주세요.");
-//            }
-//        }
-//
-//        // 예약 생성
-//        Reservation reservation = reservationService.prepareReservation(userUid, seat.getSeatId(), userService.getTimeInHours(selectedPriceType));
-//        reservationService.createReservation(reservation);
-//
-//        userService.deductTime(userUid, selectedPriceType);
-//
-//        System.out.println("예약이 완료되었습니다.");
-
-
 
         // DB 연결 닫기
         scanner.close();
--- src/main/java/com/example/user/service/UserService.java
+++ src/main/java/com/example/user/service/UserService.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class UserService {
     private UserDAO userDAO;
@@ -51,18 +52,17 @@ public class UserService {
     }
 
     // 복사3
-    public User login() throws IOException, SQLException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public User login(Scanner scanner) throws IOException, SQLException {
         List<User> loginResult = new ArrayList<User>();
         while(loginResult.size() == 0) {
             System.out.println("아이디와 비밀번호를 입력해주세요.");
             System.out.println("아이디: ");
-            String inputId = br.readLine();
+            String inputId = scanner.next();
             int inputPassword = -1;
             while (inputPassword == -1) {
                 System.out.println("비밀번호: ");
                 try {
-                    inputPassword = Integer.parseInt(br.readLine());
+                    inputPassword = scanner.nextInt();
                 } catch (NumberFormatException e) {
                     System.out.println("올바른 형식의 비밀번호를 입력해주세요.");
                     continue;
@@ -70,12 +70,11 @@ public class UserService {
                 break;
             }
 
-
             loginResult = (List<User>) userDAO.LoginValidation(inputId, inputPassword);
             if (loginResult.size() == 0) {
                 System.out.println("아이디 혹은 비밀번호가 잘못되었습니다.");
                 System.out.println("다시 입력하시겠습니까? (y/n)");
-                String answer = br.readLine().trim();
+                String answer = scanner.next();
                 System.out.println();
                 if (answer.equals("n")) {
                     System.exit(0);
